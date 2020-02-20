18fde9fc91a6f0c649425890e935f58257ad347d
wakeup <wakeupaddress@gmail.com>
2020-02-20 22:07:45 +0900

    composerの初期設定
    RYUJI_ENGINEER-31

--- public_html/index.php
+++ public_html/index.php
@@ -1,1 +1,19 @@
-<p>hello,world</p>
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use FilmDs\Database;
+
+// DB 接続の確認.
+try {
+    $DB = new Database();
+    $DB->connect();
+    $msg = "データベースへの接続確認が取れました。";
+} catch (\PDOException $e) {
+    $msg = "データベースへの接続に失敗しました。<br>(" . $e->getMessage() . ")";
+}
+
+echo $msg;
+
+// phpinfo.
+phpinfo();
