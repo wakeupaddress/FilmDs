416feec03db30ebd8210f74d4ffc5ddd2fa6e785
wakeup <wakeupaddress@gmail.com>
2020-03-01 06:19:28 +0900

    director_detailsをcssで整えた

--- public_html/director_details.php
+++ public_html/director_details.php
@@ -5,18 +5,16 @@
 <title>director details</title>
   <h1>監督詳細</h1>
   <meta name = "description" content = "監督の詳細です">
-  <style>
-  
-  </style>
+  <link rel ="stylesheet" href="css/director_details-styles.css">
 </head>
 <body>
-  <h1>是枝裕和</h1>
-  <img src="fd picture/Hirokazu,K.jpg">
-  <a><span style = color:black>監督名</span> hoge</a>
-  <a><span style = color:black>作品</span> hoge</a>
-  <a><span style = color:black>出身国</span> hoge</a>
-  <a><span style = color:black>活動期間</span> hoge</a>
-  <a class ="summary">概要</a>
+  <img src="fd-picture/Hirokazu,K.jpg" width="10%" height="10%">
+  <div style="position:absolute; top:90px; left:150px;"><span style = color:black>監督名</span> <ruby><rb>是枝裕和</rb><rp>（</rp><rt>これえだひろかず</rt><rp>）</rp></ruby></div>
+  <div style="position:absolute; top:130px; left:150px;"><span style = color:black>作品</span> hoge</div>
+  <div style="position:absolute; top:160px; left:150px;"><span style = color:black>出身国</span> hoge</div>
+  <div style="position:absolute; top:190px; left:150px;"><span style = color:black>活動期間</span> hoge</div>
+  <!-- hogeにあたる部分には登録の際にpostされた値を表示したい -->
+  </br></br><a class ="summary">概要</a>
   <p>東京都の練馬区に生まれ、9歳から清瀬市の清瀬旭が丘団地で過ごす。のちに旭が丘団地は『海よりも まだ深く』の主人公の実家として撮影場所にも使われた。鹿児島県生まれで奄美大島に渡った曽祖父、 奄美生まれで台湾に渡った祖父、台湾生まれでシベリア抑留を経験した父という家系に生まれた</p>
   <a class = "works">映画作品</a>
   <p>そして父になる（2013年）</p>
