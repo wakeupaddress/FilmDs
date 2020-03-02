5d0b5c84ae11cd3069ea05cccc97f7682a5663aa
wakeup <wakeupaddress@gmail.com>
2020-03-03 07:55:45 +0900

    検索フォームを作り、valueを設定し、preg_matchを設定した他
    phpでsql文をコーディングした(未完成）

--- public_html/search_engine.php
+++ public_html/search_engine.php
@@ -7,46 +7,53 @@
   <link rel ="stylesheet" href="css/search_engine-styles.css">
 </head>
 <body>
-
     <h1>監督検索</h1>
     <a class ="name"> 監督名 </a>
-    <!-- 変更前のコード
-    <form method="post",>
-    <p>監督名を入力してください：<input type="text" name="name"></p> -->
-    </br><input type = "text" size = "20" placeholder = "監督名を入力してください">
+    <form action ="search_result.php" method ="post">
+      <input type = "text" name="Director_name" value = "<?php echo $_POST['Director_name']?>" size = "20" placeholder = "監督名を入力してください">
+      <?php
+         mb_regex_encoding("UTF-8");
+         if (preg_match("/^[ぁ-んァ-ヶー一-龠]+$/u",$_POST['Director_name'])) {
+          // 処理を決めかねている箇所
+         }else{
+         $error = '日本語のみ使用可能です。';
+        //  処理を決めかねている箇所
+         }
+      ?>
+      <p>
+      </br><a class="sex">性別</a>
+      <label><input type="checkbox" name ="sex" value ="<?php echo $_POST['sex']?>"> 男</label>
+      <label><input type="checkbox" name ="sex" value ="<?php echo $_POST['sex']?>"> 女</label>
+      <label><input type="checkbox" name ="sex" value ="<?php echo $_POST['sex']?>"> その他</label>
+      </p>
+
+      <a class="country">出身国</a></br>
+
+      <select name="name">
+      <option name = "country" value ="<?php echo $_POST['country']?>">日本</option>
+      <option name = "country" value ="<?php echo $_POST['country']?>">アメリカ</option>
+      <option name = "country" value ="<?php echo $_POST['country']?>">フランス</option>
+      <option name = "country" value ="<?php echo $_POST['country']?>">イギリス</option>
+      <option name = "country" value ="<?php echo $_POST['country']?>">イタリア</option>
+      </select></br>
+      </br><a class="career">活動期間</a>
+      </br><input type = "text" name = "career" size= "6" placeholder = "1900" value ="<?php echo $_POST['career']?>">
+      ～ <input type = "text" name = "career"  size="6" placeholder = "2020" value ="<?php echo $_POST['career']?>">
+      
+      </br>
+      </br><a class="genre">ジャンル</a></br>
+      <label><input type="checkbox" name="genre" value ="<?php echo $_POST['genre']?>"> アクション </label>
+      <label><input type="checkbox" name="genre" value ="<?php echo $_POST['genre']?>"> SF </label>
+      <label><input type="checkbox" name="genre" value ="<?php echo $_POST['genre']?>"> サスペンス </label>
+      </br><label><input type="checkbox" name="genre" value ="<?php echo $_POST['genre']?>"> コメディ </label>
+      <label><input type="checkbox" name="genre" value ="<?php echo $_POST['genre']?>"> ホラー </label>
+      <label><input type="checkbox" name="genre" value ="<?php echo $_POST['genre']?>"> アニメ </label>
+      <label><input type="checkbox" name="genre" value ="<?php echo $_POST['genre']?>"> 恋愛 </label>
+      </p>
+      <input type="submit" name="submit" value="検索">
     </form>
-    <p>
-    </br><a class="sex">性別</a>
-
-    <p>
-    <label><input type="checkbox"> 男</label>
-    <label><input type="checkbox"> 女</label>
-    <label><input type="checkbox"> その他</label>
-    </p>
-
-    <a class="country">出身国</a></br>
-
-    <select name="name">
-    <option value="サンプル1">サンプル1</option>
-    <option value="サンプル2">サンプル2</option>
-    <option value="サンプル3">サンプル3</option>
-    </select>
-    <p>
-    </br><a class="career">活動期間</a>
-    </br><input type = "text" size= "6" placeholder = "1900"> ～ <input type = "text" size="6" placeholder = "2020">
-    <p>
-    </br><a class="genre">ジャンル</a>
-    <label><input type="checkbox" name="genre"> アクション </label>
-    <label><input type="checkbox" name="genre"> SF </label>
-    <label><input type="checkbox" name="genre"> サスペンス </label>
-    </br><label><input type="checkbox" name="genre"> コメディ </label>
-    <label><input type="checkbox" name="genre"> ホラー </label>
-    <label><input type="checkbox" name="genre"> アニメ </label>
-    <label><input type="checkbox" name="genre"> 恋愛 </label>
-    </p>
-    <a class="search" a href="">検索</a>
   <footer>
-    <a href = ""> 監督検索 </a>
+    </br><a href = "search_result.php"> 監督検索 </a>
   </footer>
 </body>
 </html>
