393e58193d77c57d6ab297af011276463733463d
wakeup <wakeupaddress@gmail.com>
2020-03-04 05:46:33 +0900

    管理画面トップページと管理登録画面のデザインをcssで整えた

---
+++ public_html/management_page.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+      <title>management_page</title>
+      <h1> FilmDs管理画面ログイン</h1>
+      <meta name = "description" content = "管理画面へのログインページです">
+      <link rel ="stylesheet" href="css/management_page-styles.css">
+    </head>
+    <body>
+    </form>
+    </body>
+</html>
---
+++ public_html/management_toppage.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+      <title>management_toppage</title>
+      <h1><a href = "">監督管理</a>　<a href ="">作品管理</a>　<a href ="">ユーザ管理</a>　　　ログイン中:</h1>
+      <meta name = "description" content = "管理画面へのログインページです">
+      <link rel ="stylesheet" href="css/management_toppage-styles.css">
+    </head>
+    <body>
+    <h2><div style="position:absolute; top:30px; left:1000px;">ログアウト</h2></div>
+    <h3>管理トップページ</h3>
+    </form>
+    </body>
+</html>
